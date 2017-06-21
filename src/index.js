a62466f28245a20b3c990add25543997c77931d0
Jeff Lau <jeff-lau@live.com>
2017-06-21 23:01:18 +0800

    Removed redaxe dep and reverted to obj

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { db } from 'redaxe'
 
 export function connect(mapToProps){
   return function(Component){
-    let props = mapToProps(db)
     let connectedComponent = () => {
-      return <Component {...props} />
+      return <Component {...mapToProps} />
     }
     return connectedComponent
   }
